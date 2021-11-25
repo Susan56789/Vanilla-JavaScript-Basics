80f9ffdb15db963587749f65020b2e73c644f14f
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-25 19:47:03 +0300

    built in objects

---
+++ javascript-objects.js
@@ -0,0 +1,13 @@
+/**
+ * BUILT IN JAVASCRIPT OBJECTS
+ */
+
+//Date
+let now = new Date ();
+//turn it into a string
+console.log(now.toDateString());
+
+
+//Math
+let id = Math.random()*100; 
+console.log(id);
